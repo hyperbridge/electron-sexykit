e33206be1471fd98d6a5868a08ab6205f491f65d
Atif Mahmud <atifmahmud95@gmail.com>
2018-11-19 20:06:42 -0800

    Electron prompt working

--- test-electron-app/main.js
+++ test-electron-app/main.js
@@ -4,8 +4,29 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
+const prompt = require('electron-prompt');
 
 let win;
+
+function createPopup(){
+    prompt({
+        title: 'Test Prompt',
+        label: "What's your name?",
+        value: 'John Doe',
+        inputAttrs: {
+            type: 'text'
+        },
+        type: 'input'
+    })
+    .then((r) => {
+        if(r === null) {
+            console.log('user cancelled');
+        } else {
+            console.log('Your name is ', r);
+        }
+    })
+    .catch(console.error);
+}
 
 function createWindow() {
     win = new BrowserWindow();
@@ -14,6 +35,7 @@ function createWindow() {
         protocol: 'file',
         slashes: true
     }));
+    createPopup();
 }
 
 // open at login
