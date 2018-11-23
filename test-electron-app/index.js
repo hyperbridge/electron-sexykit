7df50f706ff3b52e8135a910ec72d2dd3de4b979
Atif Mahmud <atifmahmud95@gmail.com>
2018-11-23 12:12:04 -0800

    Detects screen size and then zooms

--- test-electron-app/index.js
+++ test-electron-app/index.js
@@ -1,4 +1,14 @@
 console.log('Index.js');
 
 const {webFrame} = require('electron');
-webFrame.setZoomFactor(3);
+const electron = require('electron');
+const maxWidth = 1920;
+const maxHeight = 1080; 
+
+const size = electron.screen.getPrimaryDisplay().workAreaSize;
+
+if (size.width > maxWidth || size.height > maxHeight) {
+    const zoomFactor = Math.min((size.width/maxWidth), (size.height/maxHeight));
+    webFrame.setZoomFactor(zoomFactor);
+}
+
