0aaf2a2d808b70e100aada7bfd04d952cfb007db
Atif Mahmud <atifmahmud95@gmail.com>
2018-11-28 00:15:10 -0800

    cleanup

--- test-electron-app/main.js
+++ test-electron-app/main.js
@@ -31,7 +31,7 @@ function createPopup(){
 function createWindow() {
     win = new BrowserWindow();
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, './render/index.html'),
         protocol: 'file',
         slashes: true
     }));
