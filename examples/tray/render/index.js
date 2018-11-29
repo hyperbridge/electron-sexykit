b2b616c447db6be15f11480b83451c6d90b0e290
Atif Mahmud <atifmahmud95@gmail.com>
2018-11-29 01:18:12 -0800

    Restructure

---
+++ examples/tray/render/index.js
@@ -0,0 +1,10 @@
+console.log('Index.js');
+
+const {webFrame} = require('electron');
+const electron = require('electron');
+const remote = require('electron');
+const path = require('path')
+const { Tray, Menu } = require('electron').remote;
+
+
+
